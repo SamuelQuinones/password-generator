7e110a03e09683482c264130cf0827feb2174dec
Samuel Quiñones <sdquinones1@gmail.com>
2021-04-09 00:20:31 -0400

    added configs

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: ["./src/**/*.tsx", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        md: '4rem',
+        lg: '6rem',
+        xl: '7rem',
+        '2xl': '8rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
