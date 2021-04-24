b4c3eb8be0a937c40cee92cdf4ab3ebb4f3ad41d
Samuel Quiñones <sdquinones1@gmail.com>
2021-04-24 11:42:49 -0400

    added comments and ran prettier

--- src/components/ThemeSwitcher/Helper.ts
+++ src/components/ThemeSwitcher/Helper.ts
@@ -1,5 +1,18 @@
+/**
+ * Available Theme Colors
+ *
+ * Currently designed to use / implement tailwind base colors
+ */
 export type Themes = "red" | "pink" | "blue";
 
+/**
+ * returns one of the available themes - which will be added to the dom classlist
+ *
+ * used in initial dom load
+ *
+ * @param input string to check against list of themes
+ * @returns one of the available themes
+ */
 export const checkTheme = (input: string | null): Themes => {
   switch (input) {
     case "red":
--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -2,24 +2,41 @@ import { FC, useEffect, useState } from "react";
 import { Themes, checkTheme } from "./Helper";
 import { ReactComponent as Palette } from "../../assets/palette-fill.svg";
 
+/**
+ * Produces buttons that will change the theme class to match the corresponding color.
+ *
+ * Buttons are circular, and stick to the bottom right
+ */
 const ThemeSwitcher: FC = () => {
   const [theme, setTheme] = useState<Themes>("pink");
 
+  /** Runs on first mount to get the initial theme */
   useEffect(() => {
     const LS_KEY = "color-theme";
-
     const storedTheme = localStorage.getItem(LS_KEY);
-
     const _theme = checkTheme(storedTheme);
 
     setTheme(_theme);
   }, []);
 
+  /**
+   * changes the theme / dom class-list
+   *
+   * There is a `theme` state that holds the current theme,
+   * this state will be sued to remove the currnet theme after changing it to the input
+   * before setting the state to the new value.
+   *
+   * @param newTheme one of the available themes that we will change to
+   */
   const changeTheme = (newTheme: Themes) => {
+    //* just in case the theme were "changing" to is the same theme
     if (newTheme === theme) return;
 
+    //* store values
     const docBody = document.body.classList;
     const LS_KEY = "color-theme";
+    //* for some reason, animations on page load causes an odd flash
+    //* this block here will add an animation class on the first theme change
     if (!docBody.contains("animate-class")) docBody.add("animate-class");
 
     docBody.add(newTheme);
@@ -38,14 +55,14 @@ const ThemeSwitcher: FC = () => {
         <Palette className="inline-block" />
       </button>
       <button
-        className="theme-changer-red bg-red-500 border-red-300"
+        className="theme-changer-red"
         disabled={theme === "red"}
         onClick={() => changeTheme("red")}
       >
         <Palette className="inline-block" />
       </button>
       <button
-        className="theme-changer-blue bg-blue-500 border-blue-300"
+        className="theme-changer-blue"
         disabled={theme === "blue"}
         onClick={() => changeTheme("blue")}
       >
