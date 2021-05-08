289bd9047413f3d7eeae0c09f6b1c7c8e510cefa
Samuel Quiñones <sdquinones1@gmail.com>
2021-05-08 16:05:49 -0400

    added `settingsSaved`

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -4,3 +4,5 @@ import type { RootState } from "store";
 export const getSettings = (state: RootState) => state.settings;
 export const getGeneratedPW = (state: RootState) => state.settings.generatedPW;
 export const getTheme = (state: RootState) => state.settings.theme;
+export const getSettingsSaved = (state: RootState) =>
+  state.settings.settingsSaved;
--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -4,11 +4,13 @@ import { Themes } from "components/ThemeSwitcher/Helper";
 export type UserState = {
   generatedPW?: string;
   theme: Themes;
+  settingsSaved: boolean;
 };
 
 const initialState: UserState = {
   generatedPW: undefined,
   theme: "pink",
+  settingsSaved: false,
 };
 
 const slice = createSlice({
@@ -18,12 +20,16 @@ const slice = createSlice({
     reset(state) {
       state.generatedPW = undefined;
       state.theme = "pink";
+      state.settingsSaved = false;
     },
     setTheme(state, action: PayloadAction<Themes>) {
       state.theme = action.payload;
     },
     setGeneratedPW(state, action: PayloadAction<string>) {
       state.generatedPW = action.payload;
+    },
+    setSettingsSaved(state, action: PayloadAction<boolean>) {
+      state.settingsSaved = action.payload;
     },
     resetGeneratedPW(state) {
       state.generatedPW = undefined;
