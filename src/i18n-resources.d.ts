d16b994edbc1139902bd398cf3a8c4bc12b2f2fa
Samuel Quiñones <sdquinones1@gmail.com>
2021-07-13 22:26:35 -0400

    used new 11.11.1 methods

--- src/i18n-resources.d.ts
+++ src/i18n-resources.d.ts
@@ -2,7 +2,10 @@ import "react-i18next";
 import { TranslationObj } from "./lang/resources";
 
 declare module "react-i18next" {
-  export interface Resources {
-    translation: TranslationObj;
+  interface CustomTypeOptions {
+    defaultNS: "translation";
+    resources: {
+      translation: TranslationObj;
+    };
   }
 }
