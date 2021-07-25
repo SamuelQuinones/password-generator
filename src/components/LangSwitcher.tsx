ca51ec77d002549e6cdd3c7c869f84bfe5a9b9c9
Samuel Quiñones <sdquinones1@gmail.com>
2021-07-25 12:20:30 -0400

    switched from rc-dropdown to custom component

--- src/components/LangSwitcher.tsx
+++ src/components/LangSwitcher.tsx
@@ -1,35 +1,54 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { FC } from "react";
-import Dropdown from "rc-dropdown";
-import Menu, { Item } from "rc-menu";
 import Button from "./Button";
 import { Translate } from "./SVG-Icons";
 import { useTranslation } from "react-i18next";
 import { langPickerConfig } from "lang/resources";
+import useDropdownMenu from "react-accessible-dropdown-menu-hook";
 
 const LangSwitcher: FC = () => {
   const { i18n, t } = useTranslation();
-
-  const LangMenu = (
-    <Menu
-      onSelect={({ key }) => i18n.changeLanguage(key)}
-      selectedKeys={[i18n.languages[0]]}
-    >
-      {langPickerConfig.map(({ key, display }) => (
-        <Item key={key}>{display}</Item>
-      ))}
-    </Menu>
+  const { buttonProps, itemProps, isOpen, setIsOpen } = useDropdownMenu(
+    langPickerConfig.length
   );
 
+  const toggleItem = (lang: string) => {
+    setIsOpen(false);
+    i18n.changeLanguage(lang);
+  };
+
   return (
-    <div className="mb-5 text-center">
-      <Dropdown trigger={["click"]} overlay={LangMenu} animation="slide-up">
-        <Button
-          style={{ padding: "0.25rem", borderRadius: 0 }}
-          className="text-lg"
-        >
-          <Translate width="1em" height="1em" /> {t("lang_desc")}
-        </Button>
-      </Dropdown>
+    <div className="mb-5 relative flex justify-center">
+      <Button
+        style={{ padding: "0.25rem", borderRadius: 0 }}
+        className="text-lg"
+        onClick={() => setIsOpen(!isOpen)}
+        {...buttonProps}
+      >
+        <Translate width="1em" height="1em" /> {t("lang_desc")}
+      </Button>
+      <div
+        className={isOpen ? "dropdown visible" : "dropwdown hidden"}
+        role="menu"
+        style={
+          isOpen ? { top: `${buttonProps.ref.current?.offsetHeight}px` } : {}
+        }
+      >
+        {langPickerConfig.map(({ key, display }, idx) => (
+          <a
+            id={`menu-item-${idx}`}
+            key={key}
+            onClick={() => toggleItem(key)}
+            className={`block dropdown-item ${
+              i18n.language === key ? "active" : ""
+            }`.trim()}
+            {...itemProps[idx]}
+          >
+            {display}
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
